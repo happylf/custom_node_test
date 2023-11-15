1c7b3dd5ca60867cc1d28c109cef758dfd8c12d0
happylf <150868019+happylf@users.noreply.github.com>
2023-11-15 09:23:27 +0900

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .test_node import *
+
+NODE_CLASS_MAPPINGS = {
+    "TestNode01": cl_TestNode01,
+    "TestNode02": cl_TestNode02,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "TestNode01": "Test Node01",
+    "TestNode02": "Test Node02",
+}
